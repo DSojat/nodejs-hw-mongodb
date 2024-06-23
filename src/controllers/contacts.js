b8beae8031c0fc45fc482cbb1f57a6d8fa58a769
DSojat <145054801+DSojat@users.noreply.github.com>
2024-06-23 21:53:36 +0300

    step 2

--- src/controllers/contacts.js
+++ src/controllers/contacts.js
@@ -1,4 +1,5 @@
 import { getAllContacts, getContactById } from '../services/contacts.js';
+import createHttpError from 'http-errors';
 
 export const getContactsController = async (req, res) => {
   const contacts = await getAllContacts();
@@ -10,16 +11,13 @@ export const getContactsController = async (req, res) => {
   });
 };
 
-export const getContactByIdController = async (req, res) => {
+export const getContactByIdController = async (req, res, next) => {
   const { contactId } = req.params;
   const contact = await getContactById(contactId);
 
   // Відповідь, якщо контакт не знайдено
   if (!contact) {
-    res.status(404).json({
-      status: 404,
-      message: 'Contact not found',
-    });
+    next(createHttpError(404, 'Contact not found'));
     return;
   }
 
--- src/server.js
+++ src/server.js
@@ -3,6 +3,8 @@ import pino from 'pino-http';
 import cors from 'cors';
 import { env } from './utils/env.js';
 import { default as contactsRouter } from './routers/contacts.js'; // Імпортуємо роутер
+import { errorHandler } from './middlewares/errorHandler.js';
+import { notFoundHandler } from './middlewares/notFoundHandler.js';
 
 const PORT = Number(env('PORT', '3000'));
 
@@ -22,22 +24,9 @@ const setupServer = () => {
 
   server.use(contactsRouter); // Додаємо роутер до server як middleware
 
-  server.use('*', (req, res, next) => {
-    res.status(404).json({
-      status: 404,
-      message: 'Not found',
-    });
-    next();
-  });
+  server.use('*', notFoundHandler);
 
-  server.use((err, req, res, next) => {
-    res.status(500).json({
-      status: 500,
-      message: 'Something went wrong',
-      error: err.message,
-    });
-    next();
-  });
+  server.use(errorHandler);
 
   server.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
