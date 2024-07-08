f8b37b753c1e01dbd8e03154abbb8f8a42b335e8
DSojat <145054801+DSojat@users.noreply.github.com>
2024-07-08 16:34:00 +0300

    step3

--- src/controllers/auth.js
+++ src/controllers/auth.js
@@ -1,11 +1,24 @@
 import { registerUser } from '../services/auth.js';
+import { loginUser } from '../services/auth.js';
 
 export const registerUserController = async (req, res) => {
-  const user = await registerUser(req.body);
+  const newUser = await registerUser(req.body);
+
+  // дістаємо данні без паролю
+  const data = {
+    name: newUser.name,
+    email: newUser.email,
+  };
 
   res.status(201).json({
     status: 201,
     message: 'Successfully registered a user!',
-    data: user,
+    data,
   });
+};
+
+export const loginUserController = async (req, res) => {
+  await loginUser(req.body);
+
+  // далі ми доповнемо цей контролер
 };
--- src/services/auth.js
+++ src/services/auth.js
@@ -1,5 +1,29 @@
+import bcrypt from 'bcrypt';
+import createHttpError from 'http-errors';
 import UsersModel from '../db/models/User.js';
 
 export const registerUser = async payload => {
-  return await UsersModel.create(payload);
+  const user = await UsersModel.findOne({ email: payload.email });
+  if (user) throw createHttpError(409, 'Email in use');
+
+  const encryptedPassword = await bcrypt.hash(payload.password, 10);
+
+  return await UsersModel.create({
+    ...payload,
+    password: encryptedPassword,
+  });
+};
+
+export const loginUser = async payload => {
+  const user = await UsersModel.findOne({ email: payload.email });
+  if (!user) {
+    throw createHttpError(404, 'User not found');
+  }
+  const isEqual = await bcrypt.compare(payload.password, user.password); // Порівнюємо хеші паролів
+
+  if (!isEqual) {
+    throw createHttpError(401, 'Unauthorized');
+  }
+
+  // далі ми доповнемо цей контролер
 };
